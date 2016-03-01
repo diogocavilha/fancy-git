6258e6cfc1f03266c46a54ac8355585ca66782fc
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-03-01 09:38:23 -0300

    Update fancy_git.sh

--- fancy_git.sh
+++ fancy_git.sh
@@ -84,7 +84,7 @@ git_parse_branch() {
 }
 
 PS1="\e]0;\[\u\]@\[\h\]\[:\] \[\w\]\[\a\]\[${debian_chroot:+($debian_chroot)}\]\[\u\]@\[\h\]\[:\]\[\w\]\[\$\] "
-PS1="$PS1$(git_parse_branch)"
+PS1="$PS1\$(git_parse_branch)"
 
 if [ "$CONFIG_PS1_COLOR" = "true" ]; then
     PS1="$BOLD_GREEN\[\u\]$BOLD_YELLOW@$BOLD_GREEN\[\h\]$BOLD_YELLOW:\[\w\]\[\$\]$DEFAULT_COLOR $(git_parse_branch)"
