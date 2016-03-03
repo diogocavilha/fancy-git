d823777e8610fc1f316fda2113c61b2dd8cfa186
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-03-02 23:46:13 -0300

    Code refactoring.

--- main.sh
+++ main.sh
@@ -1,31 +1,31 @@
-CONFIG_GIT_BRANCH_DELIMITERS[0]="("
-CONFIG_GIT_BRANCH_DELIMITERS[1]=")"
-CONFIG_GIT_BRANCH_ICON_CHANGED="✖"   # icons: ✖, x
-CONFIG_GIT_BRANCH_ICON_UNCHANGED="✔" # icons: ✔
+FG_CONFIG_BRANCH_DELIMITERS[0]="("
+FG_CONFIG_BRANCH_DELIMITERS[1]=")"
+FG_CONFIG_BRANCH_ICON_CHANGED="✖"   # icons: ✖, x
+FG_CONFIG_BRANCH_ICON_UNCHANGED="✔" # icons: ✔
 
 source ~/.fancy-git/aliases
 
-fancygit_branch_status() {
+fg_branch_status() {
     local icon=""
     local branch_status=$(git status -s 2> /dev/null)
 
-    icon=$CONFIG_GIT_BRANCH_ICON_UNCHANGED
+    icon=$FG_CONFIG_BRANCH_ICON_UNCHANGED
 
     if [ "$branch_status" != "" ]; then
-        icon=$CONFIG_GIT_BRANCH_ICON_CHANGED
+        icon=$FG_CONFIG_BRANCH_ICON_CHANGED
     fi
 
     echo $icon
 }
 
-fancygit_branch_name() {
+fg_branch_name() {
     local branch_name=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
 
     if [ "$branch_name" != "" ]; then
-        branch_name="${CONFIG_GIT_BRANCH_DELIMITERS[0]}$branch_name${CONFIG_GIT_BRANCH_DELIMITERS[1]} $(fancygit_branch_status) "
+        branch_name="${FG_CONFIG_BRANCH_DELIMITERS[0]}$branch_name${FG_CONFIG_BRANCH_DELIMITERS[1]} $(fg_branch_status) "
     fi
 
     echo "$branch_name"
 }
 
-PS1="$PS1\$(fancygit_branch_name)"
+PS1="$PS1\$(fg_branch_name)"
