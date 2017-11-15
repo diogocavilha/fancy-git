2f2fadfbd0f5abae17ec22126bdbcbecbe754942
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-11-14 23:31:15 -0200

    Change prompt colors

--- main.sh
+++ main.sh
@@ -6,14 +6,14 @@ bold="\\[\\e[1m\\]"
 bold_none="\\[\\e[0m\\]"
 bg_none="\\[\\e[49m\\]"
 bg_dark_gray="\\[\\e[100m\\]"
-bg_yellow="\\[\\e[43m\\]"
+bg_light_yellow="\\[\\e[103m\\]"
 bg_white="\\[\\e[107m\\]"
 bg_blue="\\[\\e[44m\\]"
 dark_gray="\\[\\e[90m\\]"
 blue="\\[\\e[34m\\]"
 black="\\[\\e[30m\\]"
 white="\\[\\e[97m\\]"
-yellow="\\[\\e[33m\\]"
+light_yellow="\\[\\e[93m\\]"
 
 user_at_host="${white}${bg_dark_gray}${bold}"
 user_at_host_end="${bold_none}${bg_none}"
@@ -24,10 +24,10 @@ separator=""
 
 s_darkgray_bgblue="${dark_gray}${bg_blue}${separator}${bg_none}${none}"
 s_blue_bgdarkgray="${blue}${bg_dark_gray}${separator}${bg_none}${none}${bg_none}"
-s_darkgray_bgyellow="${dark_gray}${bg_yellow}${separator}${bg_none}${none}"
+s_darkgray_bgyellow="${dark_gray}${bg_light_yellow}${separator}${bg_none}${none}"
 s_darkgray_bgwhite="${dark_gray}${bg_white}${separator}${bg_none}${none}"
 s_darkgray="${dark_gray}${separator}${none}"
-s_yellow="${yellow}${separator}${none}"
+s_yellow="${light_yellow}${separator}${none}"
 s_white="${white}${separator}${none}"
 
 fg_prompt_builder() {
@@ -43,7 +43,7 @@ fg_prompt_builder() {
 
     if [ "$branch_status" != "" ]
     then
-        branch="${s_darkgray_bgyellow}${bg_yellow}${bold}"
+        branch="${s_darkgray_bgyellow}${bg_light_yellow}${black}${bold}"
         branch_end="${bg_none}${bold_none}${s_yellow}"
     fi
 
