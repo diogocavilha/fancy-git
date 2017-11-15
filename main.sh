9cd03053edfe6e2b95142f45a7593b095b3b0250
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-11-14 23:14:34 -0200

    Apply shellcheck revision

--- main.sh
+++ main.sh
@@ -1,64 +1,65 @@
 . ~/.fancy-git/aliases
 . ~/.fancy-git/fancygit-completion
 
-NONE="\[\e[39m\]"
-BOLD="\[\e[1m\]"
-BOLD_NONE="\[\e[0m\]"
-BG_NONE="\[\e[49m\]"
-BG_DARK_GRAY="\[\e[100m\]"
-BG_YELLOW="\[\e[43m\]"
-BG_WHITE="\[\e[107m\]"
-BG_BLUE="\[\e[44m\]"
-DARK_GRAY="\[\e[90m\]"
-BLUE="\[\e[34m\]"
-BLACK="\[\e[30m\]"
-WHITE="\[\e[97m\]"
-YELLOW="\[\e[33m\]"
+none="\\[\\e[39m\\]"
+bold="\\[\\e[1m\\]"
+bold_none="\\[\\e[0m\\]"
+bg_none="\\[\\e[49m\\]"
+bg_dark_gray="\\[\\e[100m\\]"
+bg_yellow="\\[\\e[43m\\]"
+bg_white="\\[\\e[107m\\]"
+bg_blue="\\[\\e[44m\\]"
+dark_gray="\\[\\e[90m\\]"
+blue="\\[\\e[34m\\]"
+black="\\[\\e[30m\\]"
+white="\\[\\e[97m\\]"
+yellow="\\[\\e[33m\\]"
 
-CONNECTOR=""
+user_at_host="${white}${bg_dark_gray}${bold}"
+user_at_host_end="${bold_none}${bg_none}"
+background_path="$bg_blue"
+background_user_symbol="$bg_dark_gray"
 
-C_DARKGRAY_BGBLUE="${DARK_GRAY}${BG_BLUE}${CONNECTOR}${BG_NONE}${NONE}"
-C_BLUE_BGDARKGRAY="${BLUE}${BG_DARK_GRAY}${CONNECTOR}${BG_NONE}${NONE}${BG_NONE}"
-C_BLUE_BGYELLOW="${BLUE}${BG_YELLOW}${CONNECTOR}${BG_NONE}${NONE}${BG_NONE}"
-C_DARKGRAY_BGYELLOW="${DARK_GRAY}${BG_YELLOW}${CONNECTOR}${BG_NONE}${NONE}"
-C_DARKGRAY_BGWHITE="${DARK_GRAY}${BG_WHITE}${CONNECTOR}${BG_NONE}${NONE}"
-C_DARKGRAY="${DARK_GRAY}${CONNECTOR}${NONE}"
-C_BLUE="${BLUE}${CONNECTOR}${NONE}"
-C_YELLOW="${YELLOW}${CONNECTOR}${NONE}"
-C_WHITE="${WHITE}${CONNECTOR}${NONE}"
+separator=""
 
-fg_get_branch_name() {
-    local branch_name=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-    echo "$branch_name"
-}
+s_darkgray_bgblue="${dark_gray}${bg_blue}${separator}${bg_none}${none}"
+s_blue_bgdarkgray="${blue}${bg_dark_gray}${separator}${bg_none}${none}${bg_none}"
+s_darkgray_bgyellow="${dark_gray}${bg_yellow}${separator}${bg_none}${none}"
+s_darkgray_bgwhite="${dark_gray}${bg_white}${separator}${bg_none}${none}"
+s_darkgray="${dark_gray}${separator}${none}"
+s_yellow="${yellow}${separator}${none}"
+s_white="${white}${separator}${none}"
 
-fg_branch_status() {
-    BRANCH="${C_DARKGRAY_BGWHITE}${BG_WHITE}${BLACK}${BOLD}"
-    BRANCH_END="${BG_NONE}${NONE}${BOLD_NONE}${C_WHITE}"
+fg_prompt_builder() {
+    local branch
+    local branch_end
+    local branch_name
+    local branch_status
 
-    local branch_status=$(git status -s 2> /dev/null)
+    branch="${s_darkgray_bgwhite}${bg_white}${black}${bold}"
+    branch_end="${bg_none}${none}${bold_none}${s_white}"
+    branch_name=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+    branch_status=$(git status -s 2> /dev/null)
 
-    if [ "$branch_status" != "" ]; then
-        BRANCH="${C_DARKGRAY_BGYELLOW}${BG_YELLOW}${BOLD}"
-        BRANCH_END="${BG_NONE}${BOLD_NONE}${C_YELLOW}"
+    if [ "$branch_status" != "" ]
+    then
+        branch="${s_darkgray_bgyellow}${bg_yellow}${bold}"
+        branch_end="${bg_none}${bold_none}${s_yellow}"
     fi
 
-    local branch_name=$(fg_get_branch_name)
-    local branch="${BRANCH} ${branch_name} ${BRANCH_END}"
+    PS1="${user_at_host}\\u@\\h ${user_at_host_end}${s_darkgray_bgblue}"
+    PS1=$PS1"${background_path}${bold}${white} \\w ${none}${bold_none}$s_blue_bgdarkgray"
 
-    if [ "$branch_name" != "" ]; then
-        PS1="${WHITE}${BG_DARK_GRAY}${BOLD}\u@\h ${BOLD_NONE}${BG_NONE}${C_DARKGRAY_BGBLUE}"
-        PS1=$PS1"${BG_BLUE}${BOLD}${WHITE} \w ${NONE}${BOLD_NONE}$C_BLUE_BGDARKGRAY"
-        PS1=$PS1"${BG_DARK_GRAY}${BOLD}${WHITE} \$ ${NONE}${BOLD_NONE}${BG_NONE}${NONE}"
-        PS1=$PS1"$branch "
+    if [ "$branch_name" != "" ]
+    then
+        PS1=$PS1"${background_user_symbol}${bold}${white} \$ ${none}${bold_none}${bg_none}${none}"
+        PS1=$PS1"${branch} ${branch_name} ${branch_end} "
         return
     fi
 
-    PS1="${WHITE}${BG_DARK_GRAY}${BOLD}\u@\h ${BOLD_NONE}${BG_NONE}${C_DARKGRAY_BGBLUE}"
-    PS1=$PS1"${BG_BLUE}${BOLD}${WHITE} \w ${NONE}${BOLD_NONE}$C_BLUE_BGDARKGRAY"
-    PS1=$PS1"${BG_DARK_GRAY}${BOLD}${WHITE} \$ ${NONE}${BOLD_NONE}${BG_NONE}${NONE}${C_DARKGRAY} "
+    PS1=$PS1"${background_user_symbol}${bold}${white} \$ ${none}${bold_none}${bg_none}${none}${s_darkgray} "
 }
 
-fg_branch_status
+fg_prompt_builder
 
-PROMPT_COMMAND+="fg_branch_status"
+PROMPT_COMMAND+="fg_prompt_builder"
