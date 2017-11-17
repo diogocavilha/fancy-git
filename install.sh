c9018de0d75583e358a2c62c9f37ff3bc3827683
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-11-16 22:48:53 -0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Author: Diogo Alexsander Cavilha <diogocavilha@gmail.com>
+# Date: 11.16.2017
+#
+# Fancygit installation script.
+
+git clone https://github.com/diogocavilha/fancy-git.git ~/.fancy-git
+echo ". ~/.fancy-git/main.sh" >> ~/.bashrc
+. ~/.bashrc
+
