96825cea2c8a11489d6eabac701ab0edb478c4f4
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-11-16 23:39:39 -0200

    Rename main.sh to prompt.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 git clone https://github.com/diogocavilha/fancy-git.git ~/.fancy-git
 cp -f ~/.bashrc ~/.bashrc.backup
-echo "" >> .bashrc
-echo ". ~/.fancy-git/main.sh" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo ". ~/.fancy-git/prompt.sh" >> ~/.bashrc
 . ~/.bashrc
 
