9663eab91939a33dc543deddd762463279cbdf70
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2017-11-21 15:35:14 -0200

    Improve installation script

--- install.sh
+++ install.sh
@@ -5,14 +5,24 @@
 #
 # Fancygit installation script.
 
+fg_os=$(uname)
+
 git clone https://github.com/diogocavilha/fancy-git.git ~/.fancy-git
-cp -f ~/.bashrc ~/.bashrc.backup
-echo "" >> ~/.bashrc
-echo ". ~/.fancy-git/prompt.sh" >> ~/.bashrc
+
+if [ "$fg_os" = "Linux" ]
+then
+    cp -f ~/.bashrc ~/.bashrc.backup
+    echo "" >> ~/.bashrc
+    echo ". ~/.fancy-git/prompt.sh" >> ~/.bashrc
+fi
+
+if [ "$fg_os" = "Darwin" ]
+then
+    cp -f ~/.bash_profile ~/.bash_profile.backup
+    echo "" >> ~/.bash_profile
+    echo ". ~/.fancy-git/prompt.sh" >> ~/.bash_profile
+fi
 
 mkdir ~/.fonts
 cp -i ~/.fancy-git/fonts/SourceCodePro+Powerline+Awesome+Regular.ttf ~/.fonts
 fc-cache -fv
-
-. ~/.bashrc
-
