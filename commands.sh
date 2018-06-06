d67ab4053555056e49a6ae5afa4c9f6a70c91089
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2018-06-06 13:47:22 -0300

    Add command to change mode

--- commands.sh
+++ commands.sh
@@ -28,6 +28,7 @@ fg_command_not_found() {
 }
 
 fg_change_mode() {
+    echo "default" > ~/.fancy-git/mode
     echo ""
     echo " If you cannot see any changes yet, please restart the terminal."
     echo ""
