4a9330f1290edf4df9501e6b2a4d85654ce0a681
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2018-06-11 10:39:42 -0300

    Make simple prompt work according to default function structure

--- prompt_styles/simple.sh
+++ prompt_styles/simple.sh
@@ -29,4 +29,11 @@ fg_branch_name() {
     echo "$branch_name"
 }
 
-PS1="$PS1\$(fg_branch_name)"
+fancygit_prompt_builder() {
+    . ~/.fancy-git/config
+
+    PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ $(fg_branch_name)"
+}
+
+PROMPT_COMMAND="fancygit_prompt_builder"
+
